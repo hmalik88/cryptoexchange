8cbf2e0f1e68832241760883c89c6ed2071af683
hmalik88 <hbmalik88@gmail.com>
2019-05-31 23:54:37 -0400

    landing page work

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -3,6 +3,9 @@ import '../scss/LandingPage.scss'
 
 export default class LandingPage extends React.Component {
 
+  //contructor - grab the root and give it a unique class to target the css with
+  //set state for loginAddress for the login input box
+
   constructor(props) {
     super(props);
     let root = document.querySelector('#root');
@@ -11,6 +14,8 @@ export default class LandingPage extends React.Component {
     this.state = {loginAddress: ''};
   }
 
+  //change handler for login input
+
   handleLoginAddress = e => {
     this.setState({loginAddress: e.target.value});
   }
@@ -18,28 +23,32 @@ export default class LandingPage extends React.Component {
   render() {
     return(
       <>
-        <div className="landing-title">CryptoXChange</div>
-        <div className="login-box">
-          <div className="login-header">
-            <div className="login-text">
-              Welcome! Sign In With Your MalikCoin Address
+        <div className="landing-1">
+          <div className="landing-title">CryptoXChange</div>
+        </div>
+        <div className="landing-2">
+          <div className="login-box">
+            <div className="login-header">
+              <div className="login-text">
+                Welcome! Sign In With Your MalikCoin Address
+              </div>
+            </div>
+            <div className="login-body">
+              <label className="login-address-label">
+                MalikCoin Address
+              </label>
+              <input className="login-address" type="text" value={this.state.loginAddress} onChange={this.handleLoginAddress} />
+              <div className="login-button" onClick={this.handleLogin}><div>Sign In</div></div>
             </div>
           </div>
-          <div className="login-body">
-            <label className="login-address-label">
-              MalikCoin Address
-            </label>
-            <input className="login-address" type="text" value={this.state.loginAddress} onChange={this.handleLoginAddress} />
-            <div className="login-button" onClick={this.handleLogin}>Sign In</div>
+          <div className="generate-address">
+            <div className="generate-header">
+              Generate An Address
+            </div>
+            <div type="text" className="generated-address"></div>
+            <div className="generate-btn">Generate Address</div>
+            <div className="claim-btn">Claim Address</div>
           </div>
-        </div>
-        <div className="generate-address">
-          <div className="generate-header">
-            Generate An Address
-          </div>
-          <div type="text" className="generated-address"></div>
-          <div className="generate-btn">Generate Address</div>
-          <div className="claim-btn">Claim Address</div>
         </div>
       </>
       )

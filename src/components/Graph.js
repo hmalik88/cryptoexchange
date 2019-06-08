200614cc6815889a8ab6917f143b519c2df44c1c
hmalik88 <hbmalik88@gmail.com>
2019-06-08 05:54:49 -0400

    almost done w/barchart

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -5,32 +5,57 @@ import '../scss/Graph.scss'
 
 export default class Graph extends React.Component {
 
-
-  componentDidMount() {
-    let colors = {positive: 'green', negative: 'red'}
-
-      let labels = _.union(this.props.inFlows, this.props.outFlows)
-      console.log(this.props)
-
+  computeGraph = () => {
+    let labels = _.union(Object.keys(this.props.inFlows), Object.keys(this.props.outFlows));
+    let negValues = [0, ...Object.values(this.props.outFlows)];
+    let posValues = Object.values(this.props.inFlows);
     let canvas = document.querySelector('#canvas');
     let myChart = new Chart(canvas, {
       type: 'bar',
       data: {
-        labels: '',
+        labels: labels,
         datasets: [{
-          data: []
+          label: 'Negative Cashflow',
+          data: negValues,
+          backgroundColor: 'rgb(164, 164, 164)'
+        },
+        {
+          label: 'Positive Cashflow',
+          data: posValues,
+          backgroundColor: '#FCC62C'
         }]
-      }
+      },
+      options: {
+          title: {
+            display: true,
+            text: 'Transaction History (Cash flows)'
+          },
+          tooltips: {
+            mode: 'index',
+            intersect: false
+          },
+          responsive: true,
+          scales: {
+            xAxes: [{
+              stacked: true,
+            }],
+            yAxes: [{
+              stacked: true
+            }]
+          }
+        }
     })
   }
 
+  componentDidMount() {
+    this.computeGraph();
+  }
+
+  componentDidUpdate() {
+    this.computeGraph();
+  }
 
   render() {
-
-    return(
-      <canvas id="canvas">
-
-      </canvas>
-      )
+    return(<canvas id="canvas"></canvas>)
   }
 }
